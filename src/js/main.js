ac9f45d6ef4d8f2bef780a745db55ebade6a930f
George Clark <george.clark@holidayextras.com>
2016-07-22 17:57:03 +0100

    outset scaffold

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const main = () => {
+  const helloWorld = '<h1>Hello World!!!</h1>';
+  document.getElementById('app').innerHTML = helloWorld;
+}
+
+document.addEventListener('DOMContentLoaded', main)
